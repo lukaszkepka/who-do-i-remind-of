423db9e9b6033dd6e976ac9131e8eeca5661863c
zielluk <zielluk94@outlook.com>
2019-02-03 03:07:07 +0100

    removed surname field from comparison dto

--- api/dto/comparison_dto.py
+++ api/dto/comparison_dto.py
@@ -1,13 +1,12 @@
 
 class ComparisonDTO:
 
-    def __init__(self, person_id=0, name='', surname='', image='', similarity_ratio=0.0):
+    def __init__(self, person_id=0, name='', image='', similarity_ratio=0.0):
         self.person_id = person_id
         self.name = name
-        self.surname = surname
         self.image = image
         self.similarity_ratio = similarity_ratio
 
     def __repr__(self):
-        return "<ComparisonDTO(PersonId=%ld, Name=%s, Surname=%s, Image=%s, SimilarityRatio=%s)>" \
-               % (self.person_id, self.name, self.surname, self.image, self.similarity_ratio)
+        return "<ComparisonDTO(PersonId=%ld, Name=%s, Image=%s, SimilarityRatio=%s)>" \
+               % (self.person_id, self.name, self.image, self.similarity_ratio)
