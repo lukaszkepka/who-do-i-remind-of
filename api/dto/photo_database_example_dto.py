377f3bed82ab19161af568c156caf3b40738f106
zielluk <zielluk94@outlook.com>
2019-02-19 21:45:34 +0100

    implemented endpoint for retrieving photo databases with photo examples

---
+++ api/dto/photo_database_example_dto.py
@@ -0,0 +1,7 @@
+class PhotoDatabaseExampleDto:
+
+    def __init__(self, id=0, name='', description=''):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.photos = []
